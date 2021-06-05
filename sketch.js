2218ee305dc1f3eaf1190d610bc091e7f22c3d5b
Shasita <jainshasita@gmail.com>
2021-06-05 15:57:07 +0530

    adding project

---
+++ sketch.js
@@ -0,0 +1,196 @@
+var bg, bg_img;
+var spaceShip, spaceShip_img;
+var missile1_img, missile2_img, missile3_img;
+var alien1_img, alien2_img, alien3_img;
+var aliensGrp, missilesGrp;
+var gameState = 'play';
+var gameOver_img, reset_button, reset_button_img;
+var score = 0;
+
+function preload(){
+  //background
+ bg_img = loadImage("Images/spaceBg.jpeg");
+
+//space ship
+ spaceShip_img = loadImage("Images/spaceship.png");
+
+  //missiles
+ missile1_img = loadImage("Images/misile1.png");
+ missile2_img = loadImage("Images/misile2.png");
+ missile3_img = loadImage("Images/misile3.png");
+
+ //aliens
+ alien1_img = loadImage("Images/alien1.png");
+ alien2_img = loadImage("Images/alien2.png");
+ alien3_img = loadImage("Images/alien3.png");
+
+ //gameOver
+ gameOver_img = loadImage("Images/gameOver.png");
+
+ //reset
+ reset_button_img = loadImage("Images/replay button.png");
+ 
+}
+
+function setup() {
+
+  createCanvas(displayWidth-30, displayHeight-150);
+
+//creating the background
+  bg =  createSprite(width/2, height/2);
+  bg.addImage(bg_img);
+  bg.velocityY = 3;
+  bg.scale = 2;
+
+//creating the spaceShip
+  spaceShip = createSprite(width/2, 550, 100, 100);
+  spaceShip.addImage(spaceShip_img);
+  spaceShip.scale = 0.3;
+  spaceShip.debug = false;
+  spaceShip.setCollider('circle', 0, 0, 150)
+
+  reset_button = createSprite(width/2, height/2);
+  reset_button.addImage(reset_button_img);
+  reset_button.scale = 0.8;
+  reset_button.visible = false;
+
+  aliensGrp = new Group ();
+  missilesGrp = new Group ();
+}
+
+function draw() {
+  background("peachpuff"); 
+
+  if(gameState === 'play'){
+
+    //making the background infinite
+  if(bg.y >= height){
+    bg.y = height/2;
+  }
+
+//moving the space ship left and right
+  if(keyDown(LEFT_ARROW)){
+    spaceShip.x = spaceShip.x - 5;
+  }
+
+  if(keyDown(RIGHT_ARROW)){
+    spaceShip.x = spaceShip.x + 5;
+  }
+
+//shooting the missiles after pressing the space button
+  if(keyDown('space')){
+    spawnMissiles();
+  }
+
+  //destroying the monsters when the missiles touches them
+  if(aliensGrp.isTouching(missilesGrp)){
+    aliensGrp.destroyEach();
+    missilesGrp.destroyEach();
+    score ++;
+  }
+
+//calling the user defined function
+  spawnAliens();
+
+  if(aliensGrp.isTouching(spaceShip)){
+    gameState = 'end';
+   
+  }
+}
+
+  else if(gameState == 'end'){
+    console.log('touched')
+    bg.velocityY = 0;
+    missilesGrp.setVelocityYEach(0);
+    aliensGrp.setVelocityYEach(0);
+
+    bg.x = width/2;
+    bg.y = height/2;
+    bg.addImage(gameOver_img);
+    bg.scale = 0.7;
+
+    reset_button.visible = true;
+    
+}
+
+  if(mousePressedOver(reset_button)){
+    restart();
+  }
+
+  drawSprites();
+  textSize(40);
+  fill ("white");
+  textAlign (CENTER);
+  text (score, width-200, 100);
+}
+
+function restart(){
+
+  gameState = 'play';
+
+  bg.velocityY = 3;
+  bg.addImage(bg_img);
+  bg.scale = 2;
+
+  reset_button.visible = false;
+
+  score = 0;
+  aliensGrp.destroyEach();
+  console.log('restart');
+
+}
+
+function spawnMissiles(){
+
+//creating the sprite
+    var missiles = createSprite(spaceShip.x, spaceShip.y - 50, 100, 100);
+    missiles.velocityY = -2;
+
+    var rand = Math.round(random(1,3));
+
+//assigning random images to missiles
+    switch(rand){
+      case 1 : missiles.addImage(missile1_img);
+      break;
+      case 2 : missiles.addImage(missile2_img);
+      break;
+      case 3 : missiles.addImage(missile3_img);
+      break;
+      default : break;
+    }
+
+//assigning the scale
+    missiles.scale = 0.2;
+    missiles.lifetime = -height/missiles.velocityY;
+    missilesGrp.add(missiles);
+}
+
+function spawnAliens(){
+
+  if(frameCount % 60 === 0){
+
+//creating the sprite
+    var aliens = createSprite(Math.round(random(100, width-100)), 0, 50, 50);
+    aliens.velocityY = 2;
+
+    var rand = Math.round(random(1,3));
+    
+//assigning random images to the aliens
+    switch(rand){
+      case 1 : aliens.addImage(alien1_img);
+      break;
+      case 2 : aliens.addImage(alien2_img);
+      break;
+      case 3 : aliens.addImage(alien3_img);
+      break;
+      default : break;
+    }
+
+//assigning the scale
+    aliens.scale = 0.2;
+
+    aliens.lifetime = -height/aliens.velocityY;
+    aliensGrp.add(aliens);
+
+  }
+}
